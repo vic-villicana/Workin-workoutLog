907800d2279bc28668552101c57081f96c034fca
vic-villicana <victor_u9@msn.com>
2022-06-26 13:04:13 -0700

    correct sql query for data layer method, getWorkouts

--- backend/src/v1/database/getworkouts.js
+++ backend/src/v1/database/getworkouts.js
@@ -1,7 +1,7 @@
 const db = require("../../../db")
 
 const getWorkouts = async () => {
-    const response = await db.query('SELECT * FROM exercises')
+    const response = await db.query('SELECT * FROM routines')
 
     return response
 }
