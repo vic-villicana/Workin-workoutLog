bc7bc0db82b521cdd447767b5ccfd4c494fc6f68
vic-villicana <victor_u9@msn.com>
2022-07-15 08:50:32 -0700

    update createRoutine page to conditionaly render routineList or RoutineForm

--- frontend/src/pages/CreateRoutine.js
+++ frontend/src/pages/CreateRoutine.js
@@ -1,26 +1,28 @@
 import {useState} from 'react'
+import RoutineList from '../components/RoutineList'
+import RoutineForm from '../components/RoutineForm'
+
+import workoutFinder from '../api/workoutFinder'
 
 const CreateRoutine = () => {
-    const [name, setName] = useState("")
-    const [description, setDescription] = useState("")
+    // const [name, setName] = useState("")
+    // const [description, setDescription] = useState("")
+    const [routineId, setRoutineId] = useState()
+    const [listOpen, setListOpen] = useState(false)
+
+    const OpenList = () => {
+       setListOpen(true)
+    }
+
+    const getRoutineId = (id) => {
+        setRoutineId(id)
+    }
 
     return(
         <>
-            <section className="create-routine">
-                <div className="form-container routine">
-                    <h1>New Routine</h1>
-                    <form>
-                        <div className="formFields">
-                            <label htmlFor="name"></label>
-                            <input id="name" placeholder="Name" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-                            <label htmlFor="description"></label>
-                            <input id="desription" placeholder="Description" type="text"value={description} onChange={(e) => setDescription(e.target.value)} />
-                            
-                        </div>                    
-                        <input type="submit" className="routine-submit" />
-                    </form>
-                </div>
-            </section>
+            
+            {listOpen ? <RoutineList routineId={routineId}/> : <RoutineForm OpenList={OpenList} getRoutineId={getRoutineId}/>}
+          
         </>
     )
 }
