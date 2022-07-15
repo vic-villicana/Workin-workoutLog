0760188aa567f4552236a85e7aaf8f08eff202a5
vic-villicana <victor_u9@msn.com>
2022-07-15 11:55:10 -0700

    move axios request function to parent component

--- frontend/src/pages/CreateRoutine.js
+++ frontend/src/pages/CreateRoutine.js
@@ -10,20 +10,36 @@ const CreateRoutine = () => {
     const [routineId, setRoutineId] = useState()
     const [listOpen, setListOpen] = useState(false)
 
-    const OpenList = () => {
-       setListOpen(true)
+    // const OpenList = () => {
+    //    setListOpen(true)
+    // }
+
+    // const getRoutineId = (id) => {
+    //     setRoutineId(id)
+    // }
+
+    const submitRoutine = async (e, name, description) => {
+        e.preventDefault()
+        try{
+         const response = await workoutFinder.post('/v1/myworkouts',{
+            name:name,
+            description:description
+        })        
+        setRoutineId(response.data.result.id)
+        setListOpen(true)
+       
+        }catch(err){
+            console.log(err)
+        }
     }
 
-    const getRoutineId = (id) => {
-        setRoutineId(id)
-    }
 
     return(
-        <>
+        <div className="routinepage-container">
             
-            {listOpen ? <RoutineList routineId={routineId}/> : <RoutineForm OpenList={OpenList} getRoutineId={getRoutineId}/>}
+            {listOpen ? <RoutineList routineId={routineId}/> : <RoutineForm submitRoutine={submitRoutine} />}
           
-        </>
+        </div>
     )
 }
 export default CreateRoutine
